10c6bf91d882de59ad06071e2ceb054b1312f81d
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-01-16 22:03:19 +0330

    Update Template

--- Ashley.py
+++ Ashley.py
@@ -5,12 +5,12 @@
 +---------------------------------------+
 | Project Name : Ashley                 |
 | Licence : MIT                         |
-| Verson : 0.1.5                        |
+| Verson : 0.1.6                        |
 |                                       |
 | Developer : Amirhossein Mohammsdi     |
 | Github    : github.com/BlackIQ/Ashley |
 |                                       |
-| Last Update : Jan , 16 , 2021         |
+| Last Update : Jan , 17 , 2021         |
 +---------------------------------------+
 
 """
